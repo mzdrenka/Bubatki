75fc4e2700537d26ed97bf014337ebac3bdd5eea
mzdrenka <zdrenka.mateusz@gmail.com>
2020-11-03 02:56:08 +0100

    inicjacja

--- bubatki/bubatki/settings.py
+++ bubatki/bubatki/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from configobj import ConfigObj
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,12 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'django.contrib.postgres'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +76,26 @@ WSGI_APPLICATION = 'bubatki.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# Database configuration file location
+DB_CONF_FILE = f'{BASE_DIR}/bubatki/db.conf'
+
+# Read the configurations from file
+DB_CONFIG = ConfigObj(DB_CONF_FILE)
+
+DB_HOST = DB_CONFIG['DB_HOST']
+DB_NAME = DB_CONFIG['DB_NAME']
+DB_USER = DB_CONFIG['DB_USER']
+DB_PASSWORD = DB_CONFIG['DB_PASSWORD']
+DB_PORT = DB_CONFIG['DB_PORT']
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
     }
 }
 
